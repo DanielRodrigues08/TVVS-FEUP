8c0954b255bd24ba92ee3d53ad31cfa1eaf697e5
Daniel Rodrigues <up202006562@up.pt>
2025-01-03 01:11:51 +0000

    added controller menu tests

--- project-space-invaders/src/test/java/SpaceInvaders/Controller/Menu/GameOverControllerTest.java
+++ project-space-invaders/src/test/java/SpaceInvaders/Controller/Menu/GameOverControllerTest.java
@@ -7,7 +7,8 @@ import SpaceInvaders.Model.Sound.Sound_Options;
 import SpaceInvaders.State.GameStates;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -15,16 +16,25 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class GameOverControllerTest {
-    /*private static SoundManager mockSoundManager;
+    private static SoundManager mockSoundManager;
     private static MockedStatic<SoundManager> mockedStatic;
-    @Mock private Game game;
+    @Mock
+    private Game game;
+    @Mock
+    private BufferedWriter mockBuffer;
+    @Mock
+    private File mockFile;
 
     @BeforeAll
     static void setup() {
@@ -38,68 +48,104 @@ public class GameOverControllerTest {
         mockedStatic.close();
     }
 
-    @AfterEach
-    void clearInvocations() {
-        clearInvocations(mockSoundManager);
-        clearInvocations(game);
-    }
+    private static Stream<Arguments> valuesTestStep() {
+        GameOverMenu menu1 = mock(GameOverMenu.class);
+        when(menu1.isSelectedRestart()).thenReturn(true);
+        when(menu1.isSelectedLeaderboard()).thenReturn(false);
+        when(menu1.isSelectedExit()).thenReturn(false);
+        when(menu1.getScore()).thenReturn(0);
+        when(menu1.getUsername()).thenReturn("test");
 
-    private static Stream<Arguments> testStepParams() {
-        KeyStroke keyUp = mock(KeyStroke.class);
-        when(keyUp.getKeyType()).thenReturn(KeyType.ArrowUp);
+        GameOverMenu menu2 = mock(GameOverMenu.class);
+        when(menu2.isSelectedRestart()).thenReturn(false);
+        when(menu2.isSelectedLeaderboard()).thenReturn(true);
+        when(menu2.isSelectedExit()).thenReturn(false);
+        when(menu2.getScore()).thenReturn(0);
+        when(menu2.getUsername()).thenReturn("test");
 
-        KeyStroke keyDown = mock(KeyStroke.class);
-        when(keyDown.getKeyType()).thenReturn(KeyType.ArrowDown);
+        GameOverMenu menu3 = mock(GameOverMenu.class);
+        when(menu3.isSelectedRestart()).thenReturn(false);
+        when(menu3.isSelectedLeaderboard()).thenReturn(false);
+        when(menu3.isSelectedExit()).thenReturn(true);
+        when(menu3.getScore()).thenReturn(0);
+        when(menu3.getUsername()).thenReturn("test");
 
-        KeyStroke keyEnter = mock(KeyStroke.class);
-        when(keyEnter.getKeyType()).thenReturn(KeyType.Enter);
+        GameOverMenu menu4 = mock(GameOverMenu.class);
+        when(menu4.isSelectedRestart()).thenReturn(false);
+        when(menu4.isSelectedLeaderboard()).thenReturn(false);
+        when(menu4.isSelectedExit()).thenReturn(false);
+        when(menu4.getScore()).thenReturn(0);
+        when(menu4.getUsername()).thenReturn("test");
 
-        GameOverMenu menu1 = new GameOverMenu(100);
-        menu1.setSelectedRestartButton();
-        GameOverMenu menu2 = new GameOverMenu(100);
-        menu2.setSelectedExitButton();
-        GameOverMenu menu3 = new GameOverMenu(100);
-        menu3.setSelectedLeaderboardButton();
+
+        KeyStroke key1 = mock(KeyStroke.class);
+        when(key1.getKeyType()).thenReturn(KeyType.ArrowUp);
+
+        KeyStroke key2 = mock(KeyStroke.class);
+        when(key2.getKeyType()).thenReturn(KeyType.ArrowDown);
+
+        KeyStroke key3 = mock(KeyStroke.class);
+        when(key3.getKeyType()).thenReturn(KeyType.Enter);
+
+        KeyStroke key4 = mock(KeyStroke.class);
+        when(key4.getKeyType()).thenReturn(KeyType.ArrowRight);
+
+        KeyStroke key5 = mock(KeyStroke.class);
+        when(key5.getKeyType()).thenReturn(KeyType.Character);
+        when(key5.getCharacter()).thenReturn('a');
+
+        KeyStroke key6 = mock(KeyStroke.class);
+        when(key6.getKeyType()).thenReturn(KeyType.Backspace);
 
         return Stream.of(
-            Arguments.of(keyUp, menu1, GameStates.START_MENU, false, 0),
-            Arguments.of(keyDown, menu1, GameStates.START_MENU, false, 0),
-            Arguments.of(keyEnter, menu1, GameStates.NEW_GAME, true, 1),
-            Arguments.of(keyEnter, menu2, GameStates.START_MENU, true, 1),
-            Arguments.of(keyEnter, menu3, GameStates.LEADERBOARD, false, 0),
-            Arguments.of(null, menu1, GameStates.START_MENU, false, 0)
+                Arguments.of(key1, 1, 0, 1, menu4, GameStates.START_MENU, 0, 0, 0),
+                Arguments.of(key2, 1, 1, 0, menu4, GameStates.START_MENU, 0, 0, 0),
+                Arguments.of(key3, 0, 0, 0, menu1, GameStates.NEW_GAME, 1, 0, 0),
+                Arguments.of(key3, 0, 0, 0, menu2, GameStates.LEADERBOARD, 1, 0, 0),
+                Arguments.of(key3, 0, 0, 0, menu3, GameStates.START_MENU, 1, 0, 0),
+                Arguments.of(key3, 0, 0, 0, menu4, GameStates.QUIT_GAME, 0, 0, 0),
+                Arguments.of(key3, 0, 0, 0, menu4, GameStates.QUIT_GAME, 0, 0, 0),
+                Arguments.of(null, 0, 0, 0, menu4, GameStates.START_MENU, 0, 0, 0),
+                Arguments.of(key4, 0, 0, 0, menu4, GameStates.START_MENU, 0, 0, 0),
+                Arguments.of(key5, 0, 0, 0, menu4, GameStates.START_MENU, 0, 1, 0),
+                Arguments.of(key6, 0, 0, 0, menu4, GameStates.START_MENU, 0, 0, 1)
+        );
+    }
+
+    private static Stream<Arguments> testUpdateLeaderboardValues() {
+        return Stream.of(
+                Arguments.of("test", 0, "test 0\n"),
+                Arguments.of("", 100, "Unknow 100\n")
         );
     }
 
     @ParameterizedTest
-    @MethodSource("testStepParams")
-    void testStep(KeyStroke key, GameOverMenu menu, GameStates expectedState,
-                 boolean shouldUpdateLeaderboard, int soundEffects) throws Exception {
-        GameOverController controller = spy(new GameOverController(menu));
-        doNothing().when(controller).updateLeaderboard(anyInt(), anyString());
+    @MethodSource("valuesTestStep")
+    void testStep(KeyStroke key, int numSoundSwitch, int nextOption, int previousOption, GameOverMenu menu, GameStates state, int numState, int numAddLetter, int numRemoveLetter) {
+        GameOverController controller = new GameOverController(menu);
+        clearInvocations(menu, game, mockSoundManager);
 
-        controller.step(game, key, 0);
+        assertDoesNotThrow(() -> controller.step(game, key, 0));
 
-        if (shouldUpdateLeaderboard) {
-            verify(controller, times(1)).updateLeaderboard(anyInt(), anyString());
-        } else {
-            verify(controller, never()).updateLeaderboard(anyInt(), anyString());
-        }
-        verify(mockSoundManager, times(soundEffects)).playSound(Sound_Options.MENU_SWITCH);
-        verify(game, times(expectedState != GameStates.START_MENU ? 1 : 0))
-            .setState(expectedState);
+        verify(mockSoundManager, times(numSoundSwitch)).playSound(Sound_Options.MENU_SWITCH);
+        verify(menu, times(nextOption)).nextOption();
+        verify(menu, times(previousOption)).previousOption();
+        verify(menu, times(numAddLetter)).addLetter(any());
+        verify(menu, times(numRemoveLetter)).removeLetter();
+        assertDoesNotThrow(() -> verify(game, times(numState)).setState(any()));
+        assertDoesNotThrow(() -> verify(game, times(numState)).setState(state));
     }
 
-    @Test
-    void testCharacterInput() throws Exception {
-        GameOverMenu menu = new GameOverMenu(100);
-        GameOverController controller = new GameOverController(menu);
-        KeyStroke key = mock(KeyStroke.class);
-        when(key.getKeyType()).thenReturn(KeyType.Character);
-        when(key.getCharacter()).thenReturn('A');
+    @ParameterizedTest
+    @MethodSource("testUpdateLeaderboardValues")
+    void testUpdateLeaderboard(String userName, int score, String expectedLine) throws IOException {
+        GameOverController controller = spy(new GameOverController(new GameOverMenu(100)));
+        doReturn(mockBuffer).when(controller).createBuffer(any());
 
-        controller.step(game, key, 0);
+        controller.updateLeaderboard(score, userName);
 
-        assertEquals("A", menu.getUsername());
-    }*/
+        verify(mockBuffer).write(expectedLine);
+        verify(mockBuffer).flush();
+        verify(mockBuffer).close();
+    }
 }
---
+++ project-space-invaders/src/test/java/SpaceInvaders/Controller/Menu/PauseMenuControllerTest.java
@@ -0,0 +1,117 @@
+package SpaceInvaders.Controller.Menu;
+
+import SpaceInvaders.Controller.Sound.SoundManager;
+import SpaceInvaders.Game;
+import SpaceInvaders.Model.Menu.PauseMenu;
+import SpaceInvaders.Model.Sound.Sound_Options;
+import SpaceInvaders.State.GameStates;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PauseMenuControllerTest {
+
+    private static SoundManager mockSoundManager;
+    private static MockedStatic<SoundManager> mockedStatic;
+    @Mock
+    private Game game;
+
+    private static Stream<Arguments> valuesTestStep() {
+        PauseMenu menu1 = mock(PauseMenu.class);
+        when(menu1.isSelectedContinue()).thenReturn(true);
+        when(menu1.isSelectedRestart()).thenReturn(false);
+        when(menu1.isSelectedInstructions()).thenReturn(false);
+        when(menu1.isSelectedExit()).thenReturn(false);
+
+        PauseMenu menu2 = mock(PauseMenu.class);
+        when(menu2.isSelectedContinue()).thenReturn(false);
+        when(menu2.isSelectedRestart()).thenReturn(true);
+        when(menu2.isSelectedInstructions()).thenReturn(false);
+        when(menu2.isSelectedExit()).thenReturn(false);
+
+        PauseMenu menu3 = mock(PauseMenu.class);
+        when(menu3.isSelectedContinue()).thenReturn(false);
+        when(menu3.isSelectedRestart()).thenReturn(false);
+        when(menu3.isSelectedInstructions()).thenReturn(true);
+        when(menu3.isSelectedExit()).thenReturn(false);
+
+        PauseMenu menu4 = mock(PauseMenu.class);
+        when(menu4.isSelectedContinue()).thenReturn(false);
+        when(menu4.isSelectedRestart()).thenReturn(false);
+        when(menu4.isSelectedInstructions()).thenReturn(false);
+        when(menu4.isSelectedExit()).thenReturn(true);
+
+        PauseMenu menu5 = mock(PauseMenu.class);
+        when(menu5.isSelectedContinue()).thenReturn(false);
+        when(menu5.isSelectedRestart()).thenReturn(false);
+        when(menu5.isSelectedInstructions()).thenReturn(false);
+        when(menu5.isSelectedExit()).thenReturn(false);
+
+        KeyStroke key1 = mock(KeyStroke.class);
+        when(key1.getKeyType()).thenReturn(KeyType.ArrowUp);
+
+        KeyStroke key2 = mock(KeyStroke.class);
+        when(key2.getKeyType()).thenReturn(KeyType.ArrowDown);
+
+        KeyStroke key3 = mock(KeyStroke.class);
+        when(key3.getKeyType()).thenReturn(KeyType.Enter);
+
+        KeyStroke key4 = mock(KeyStroke.class);
+        when(key4.getKeyType()).thenReturn(KeyType.ArrowRight);  // Fixed: changed key3 to key4
+
+        return Stream.of(
+                Arguments.of(key1, 1, 0, 1, menu5, GameStates.START_MENU, 0),
+                Arguments.of(key2, 1, 1, 0, menu5, GameStates.START_MENU, 0),
+                Arguments.of(key3, 0, 0, 0, menu1, GameStates.RESUME_GAME, 1),
+                Arguments.of(key3, 0, 0, 0, menu2, GameStates.NEW_GAME, 1),
+                Arguments.of(key3, 0, 0, 0, menu3, GameStates.INSTRUCTIONS, 1),
+                Arguments.of(key3, 0, 0, 0, menu4, GameStates.START_MENU, 1),
+                Arguments.of(key3, 0, 0, 0, menu5, GameStates.QUIT_GAME, 0),
+                Arguments.of(null, 0, 0, 0, menu5, GameStates.START_MENU, 0),  // Fixed: changed key3 to null for START_MENU case
+                Arguments.of(key4, 0, 0, 0, menu5, GameStates.START_MENU, 0)   // Fixed: changed menu4 to menu5 for consistency
+        );
+    }
+
+    @BeforeAll
+    static void setup() {
+        mockSoundManager = mock(SoundManager.class);
+        mockedStatic = mockStatic(SoundManager.class);
+        mockedStatic.when(SoundManager::getInstance).thenReturn(mockSoundManager);
+    }
+
+    @AfterAll
+    static void cleanup() {
+        mockedStatic.close();
+    }
+
+    @ParameterizedTest
+    @MethodSource("valuesTestStep")
+    void testStep(KeyStroke key, int numSoundSwitch, int nextOption, int previousOption, PauseMenu menu, GameStates state, int numState) {
+        PauseMenuController controller = new PauseMenuController(menu);
+        clearInvocations(menu, game, mockSoundManager);
+
+        assertDoesNotThrow(() -> controller.step(game, key, 0));
+
+        verify(mockSoundManager, times(numSoundSwitch)).playSound(Sound_Options.MENU_SWITCH);
+        verify(menu, times(nextOption)).nextOption();
+        verify(menu, times(previousOption)).previousOption();
+        assertDoesNotThrow(() -> verify(game, times(numState)).setState(any()));
+        assertDoesNotThrow(() -> verify(game, times(numState)).setState(state));
+    }
+}
